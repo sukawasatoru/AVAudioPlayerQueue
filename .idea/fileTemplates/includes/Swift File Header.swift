2bca551e656213cb40748830646a0a3254183568
Satoru Sukawa <sukawasatoru.github@outlook.jp>
2020-04-25 16:27:32 +0900

    Add some files

--- .idea/fileTemplates/includes/Swift File Header.swift
+++ .idea/fileTemplates/includes/Swift File Header.swift
@@ -1,4 +1,5 @@
-// Copyright 2020 sukawasatoru
+#if ($HEADER_COMMENTS)
+// Copyright $YEAR sukawasatoru
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,16 +12,4 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-import UIKit
-
-class ViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    @IBAction
-    func onClickButton() {
-        print("onClick")
-    }
-}
+#end

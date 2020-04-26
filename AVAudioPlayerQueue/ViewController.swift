7aaf0b65117ec1b3bc72af7a70f20af45a8ffc19
Satoru Sukawa <sukawasatoru.github@outlook.jp>
2020-04-27 00:26:36 +0900

    Add the audio category support

--- AVAudioPlayerQueue/ViewController.swift
+++ AVAudioPlayerQueue/ViewController.swift
@@ -12,16 +12,39 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
-    var audioSE1AudioSet: AudioSet!
-    var audioSE2AudioSet: AudioSet!
-    var queueSE1AudioSet: AudioSet!
-    var queueSE2AudioSet: AudioSet!
+    @IBOutlet
+    var audioCategoryButton: UIButton!
+
+    private var audioCategorySheet: UIAlertController!
+    private var audioSE1AudioSet: AudioSet!
+    private var audioSE2AudioSet: AudioSet!
+    private var queueSE1AudioSet: AudioSet!
+    private var queueSE2AudioSet: AudioSet!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        audioCategoryButton.setTitle(AudioCategory.soloAmbient.rawValue, for: .normal)
+        audioCategorySheet = UIAlertController(title: "AudioCategory", message: nil, preferredStyle: .actionSheet)
+
+        AudioCategory.allCases.makeIterator().map { entry in
+            UIAlertAction(title: entry.rawValue, style: .default) { _ in
+                do {
+                    try AVAudioSession.sharedInstance().setCategory(entry.getCategory(), mode: .default, options: [])
+                    self.audioCategoryButton.setTitle(entry.rawValue, for: .normal)
+                } catch let err {
+                    log.warning("failed to set audio category: \(err)")
+                }
+            }
+        }.forEach { data in
+            audioCategorySheet.addAction(data)
+        }
+
+        audioCategorySheet.addAction(UIAlertAction(title: "Cancel", style: .cancel))
 
         do {
             let se1List = (1...10).map { data in
@@ -41,7 +64,14 @@ class ViewController: UIViewController {
     }
 
     @IBAction
-    func onAudioSE1Clicked() {
+    private func onAudioCategoryClicked() {
+        log.debug("onAudioCategoryClicked")
+
+        present(audioCategorySheet, animated: true)
+    }
+
+    @IBAction
+    private func onAudioSE1Clicked() {
         log.debug("onAudioSE1Clicked")
 
         audioSE1AudioSet.stop()
@@ -49,7 +79,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction
-    func onAudioSE2Clicked() {
+    private func onAudioSE2Clicked() {
         log.debug("onAudioSE2Clicked")
 
         audioSE2AudioSet.stop()
@@ -57,7 +87,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction
-    func onQueueSE1Clicked() {
+    private func onQueueSE1Clicked() {
         log.debug("onQueueSE1Clicked")
 
         queueSE1AudioSet.stop()
@@ -65,10 +95,36 @@ class ViewController: UIViewController {
     }
 
     @IBAction
-    func onQueueSE2Clicked() {
+    private func onQueueSE2Clicked() {
         log.debug("onQueueSE2Clicked")
 
         queueSE2AudioSet.stop()
         queueSE2AudioSet.play()
+    }
+}
+
+private enum AudioCategory: String, CaseIterable {
+    case ambient = "ambient"
+    case soloAmbient = "soloAmbient"
+    case playback = "playback"
+    case record = "record"
+    case playAndRecord = "playAndRecord"
+    case multiRoute = "multiRoute"
+
+    func getCategory() -> AVAudioSession.Category {
+        switch self {
+        case .ambient:
+            return AVAudioSession.Category.ambient
+        case .soloAmbient:
+            return AVAudioSession.Category.soloAmbient
+        case .playback:
+            return AVAudioSession.Category.playback
+        case .record:
+            return AVAudioSession.Category.record
+        case .playAndRecord:
+            return AVAudioSession.Category.playAndRecord
+        case .multiRoute:
+            return AVAudioSession.Category.multiRoute
+        }
     }
 }
